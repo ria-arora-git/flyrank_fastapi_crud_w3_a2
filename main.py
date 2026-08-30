011eaac92e9e139734ac840230867c37688041ea
Ria Arora <ria232014@gmail.com>
2026-08-30 17:40:11 +0530

    Stage 0: setup server and supabase client

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import sqlite3
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+print("Server running and connected to Supabase")
 
 app = FastAPI()
 
